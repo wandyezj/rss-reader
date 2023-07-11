3e3c13512d035a1cb6f64418b1325a764d8d690d
Oliver Waller <olaydubproject@gmail.com>
2023-07-11 09:14:56 -0700

    add
    404 error and MIME type exception for parser.js occuring

---
+++ src/parser.js
@@ -0,0 +1,17 @@
+//const RSS_URL = 'https://feeds.npr.org/1001/rss.xml';
+
+
+//retrieve RSS feed from specified URL, processes response, logs parsed XML
+//data to console
+function fetchAndParseRSS(RSS_URL) {
+    fetch(RSS_URL)
+      .then(response => response.text())
+      .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+      .then(data => {
+        const insertDiv = document.getElementById("insert");
+        insertDiv.textContent = new XMLSerializer().serializeToString(data);
+      })
+      .catch(error => console.error(error));
+  }
+
+module.exports = parser;
