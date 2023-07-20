4e17429b56b8e87abe9d120ff679ca65bc0fff27
wandyezj <wandyezj@gmail.com>
2023-07-20 12:40:40 -0700

    test example

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,14 @@ console.log(website());
 clock();
 console.log(website());
 
-export let lastParsed: RssItem[] = [];
-async function test(testUrl: string) {
+export async function test(testUrl: string) {
     console.log("test");
-    //debugger;
+    debugger;
 
     const items = await fetchAndParseRss(testUrl);
     console.log(items);
-    lastParsed = items;
+    localStorage.setItem("test", JSON.stringify(items));
+
     return items;
 }
 
@@ -22,13 +22,15 @@ async function test(testUrl: string) {
 const testUrl = "test-xml/npr.xml";
 test(testUrl);
 
-addFeed("https://feeds.npr.org/1001/rss.xml");
-setActiveFeed("https://feeds.npr.org/1001/rss.xml");
-markItemAsViewed("https://feeds.npr.org/1001/rss.xml", "Item 1");
+function test2() {
+    addFeed("https://feeds.npr.org/1001/rss.xml");
+    setActiveFeed("https://feeds.npr.org/1001/rss.xml");
+    markItemAsViewed("https://feeds.npr.org/1001/rss.xml", "Item 1");
 
-const currentState = getFeedState();
-console.log(currentState);
+    const currentState = getFeedState();
+    console.log(currentState);
 
-saveSettings(currentState);
-const loadedSettings = loadSettings();
-console.log(loadedSettings);
+    saveSettings(currentState);
+    const loadedSettings = loadSettings();
+    console.log(loadedSettings);
+}
