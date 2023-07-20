53ac6abcf55cac048671e3d09a3547043c9dc52d
abigel-asteraye <aster22a@mtholyoke.edu>
2023-07-20 10:29:56 -0400

    updated the DisplayFeed and RSSFeedInterface

---
+++ src/UI/feedData.interface.ts
@@ -0,0 +1,5 @@
+interface RSSFeedData {
+    url: string;
+    items: string[];
+  }
+  
--- src/UI/feedDisplay.ts
+++ src/UI/feedDisplay.ts
@@ -1,37 +1,52 @@
 // displayFeeds.ts
 import { addFeed, getFeedState } from "../FeedState/FeedState";
-async function displayFeeds() {
-    console.log("Please tell me your working");
-    try {
-        // Fetch feed data from an API or source
-        const response = await fetch("parser_URL");
-        const data = await response.json();
+import { fetchAndParseRSS } from "./parser.ts";
 
-        // Add the feed to the FeedState
-        addFeed(data.url, data.items);
+async function displayFeeds(RSS_URL: string): Promise<void> {
+  console.log("Loading");
+  try {
+    // Fetch feed data from an API or source
+    const feedData = await fetchFeedData(RSS_URL);
 
-        // Update the HTML with the new feed items
-        const feedState = getFeedState();
-        const articleListElement = document.querySelector(".article-items");
-        if (articleListElement) {
-            articleListElement.innerHTML = ""; // Clear existing items
+    // Add the feed to the FeedState
+    addFeed(feedData.url, feedData.items);
 
-            // Loop through the feed items and create list items for each
-            feedState.feeds.forEach((feed) => {
-                feed.items.forEach((item) => {
-                    const listItem = document.createElement("li");
-                    const link = document.createElement("a");
-                    link.href = "#";
-                    link.textContent = item;
-                    listItem.appendChild(link);
-                    articleListElement.appendChild(listItem);
-                });
-            });
-        }
-    } catch (error) {
-        console.error("Error fetching or displaying feed data:", error);
-    }
+    // Get the updated feed items from the FeedState
+    const feedState = getFeedState();
+    const feedItems = feedState.feeds.flatMap((feed) => feed.items);
+
+    // Update the HTML with the new feed items
+    displayFeedItems(feedItems);
+  } catch (error) {
+    console.error("Error displaying feed data:", error);
+  }
 }
 
-// Export the function so it can be used in other files
+async function fetchFeedData(RSS_URL: string): Promise<RSSFeedData> {
+  try {
+    const response = await fetch(RSS_URL);
+    const data = await response.json();
+    return data as RSSFeedData;
+  } catch (error) {
+    throw new Error("Error fetching feed data");
+  }
+}
+
+function displayFeedItems(feedItems: string[]): void {
+  const articleListElement = document.querySelector(".article-items");
+  if (articleListElement) {
+    articleListElement.innerHTML = ""; // Clear existing items
+
+    // Loop through the feed items and create list items for each
+    feedItems.forEach((item) => {
+      const listItem = document.createElement("li");
+      const link = document.createElement("a");
+      link.href = "#";
+      link.textContent = item;
+      listItem.appendChild(link);
+      articleListElement.appendChild(listItem);
+    });
+  }
+}
+
 export { displayFeeds };
