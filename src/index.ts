66e08ba2b092de5e868515534052157a0fdb764f
liu1597 <48242495+liu1597@users.noreply.github.com>
2023-07-29 09:02:20 +0930

    merge

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,31 @@ addFeed("https://feeds.npr.org/1001/rss.xml");
 setActiveFeed("https://feeds.npr.org/1001/rss.xml");
 markItemAsViewed("https://feeds.npr.org/1001/rss.xml", "Item 1");
 
+<<<<<<< Updated upstream
 const currentState = getFeedState();
 console.log(currentState);
+=======
+function initialize() {
+    // Add feed button
+    document.getElementById("add-feed-button")?.addEventListener("click", async () => {
+        debugger;
+        try {
+            // Add multiple feeds
+            const feedUrls = [
+                "https://feeds.npr.org/1001/rss.xml",
+                "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
+                "https://feeds.foxnews.com/foxnews/latest",
+                "https://www.cbsnews.com/latest/rss/main",
+                "http://rss.cnn.com/rss/cnn_topstories.rss",
+            ];
+            for (const url of feedUrls) {
+                await addFeed(url);
+            }
+        } catch (error) {
+            console.error("Failed to add feed", error);
+        }
+    });
+>>>>>>> Stashed changes
 
 saveSettings(currentState);
 const loadedSettings = loadSettings();
---
+++ tests/parse.test.ts
@@ -0,0 +1,48 @@
+import { test, expect, Page } from "@playwright/test";
+import { navigateToMainPage } from "./navigateToMainPage";
+
+test("load rss", async ({ browser }) => {
+    const page = await navigateToMainPage(browser);
+    // Give it time to parse the feed, find a better way to do this.
+    await page.waitForTimeout(2000);
+    const testData = await page.evaluate(() =>
+        eval(`
+    (() => {
+        debugger;
+        return localStorage.getItem('test');
+    })()`)
+    );
+
+    const items = JSON.parse(testData);
+    expect(items.length).toBeGreaterThan(0);
+});
+
+async function waitForLoad(page: Page) {
+    // Give it time to parse the feed, find a better way to do this.
+    await page.waitForTimeout(2000);
+}
+
+// function setTestId() {
+//     selectors.setTestIdAttribute("id");
+// }
+
+test("Add Feed Button", async ({ browser }) => {
+    const page = await navigateToMainPage(browser);
+
+    await waitForLoad(page);
+
+    await page.click("#add-feed-button");
+
+    // Get the state
+    const stateData = await page.evaluate(() =>
+        eval(`
+    (() => {
+        debugger;
+        return localStorage.getItem('test');
+    })()`)
+    );
+
+    const state = JSON.parse(stateData);
+
+    expect(state.length).toBeGreaterThan(0);
+});
