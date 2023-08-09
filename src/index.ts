a0046936e6465271c46ba0223baacae4d15594b7
jianqinL <48242495+liu1597@users.noreply.github.com>
2023-08-09 13:53:55 +0930

    Add Feed Input

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { fetchAndParseRss } from "./Parser/fetchAndParseRss";
 import { refreshFeed } from "./State/State";
 import { displayFeed, displayExpandedArticle, addFeedClickedEvent } from "./UI/displayFeed";
 import { refreshFeeds } from "./refreshFeeds";
@@ -6,6 +7,7 @@ import { registerButtonRefresh } from "./registerButtonRefresh";
 import { registerButtonStateDownload } from "./registerButtonStateDownload";
 import { registerButtonStateUpload } from "./registerButtonStateUpload";
 import { website, clock } from "./website";
+import { addFeed } from "./State/State";
 
 console.log(website());
 clock();
@@ -25,6 +27,23 @@ function initialize() {
     refreshFeeds();
     // Add event listeners to feed links
     addFeedClickedEvent();
-}
 
+    // Event listener for the "Add RSS Feed" button
+    document.getElementById("button-add-feed")?.addEventListener("click", async () => {
+        const urlInput = document.getElementById("add-feed-input") as HTMLInputElement;
+        const url = urlInput.value.trim();
+        if (url) {
+            try {
+                const rssItems = await fetchAndParseRss(url);
+                addFeed(url, rssItems);
+                console.log(`RSS feed from ${url} has been added.`);
+                displayFeed(); // Refresh the feed display
+            } catch (error) {
+                console.error(`Failed to add RSS feed from ${url}:`, error);
+            }
+        } else {
+            console.error("No URL was entered.");
+        }
+    });
+}
 initialize();
